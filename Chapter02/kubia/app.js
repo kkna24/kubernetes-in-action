3d32c2c7686b4e6aad5c3e8c198f2c98c75e4b25
Kiranpreet Kaur <163906474+kkna24@users.noreply.github.com>
2024-06-08 21:22:16 -0400

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("You've hit " + os.hostname() + "\n" + "this is second version of the app");
 };
 
 var www = http.createServer(handler);
